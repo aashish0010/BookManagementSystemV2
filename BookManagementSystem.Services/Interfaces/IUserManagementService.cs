e270ed91b61f8ce0fa8129f37d3e60115b3e690e
aashish pudasaini <pudasainiaashish2@gmail.com>
2023-08-28 21:12:15 +0545

    .

--- BookManagementSystem.Services/Interfaces/IUserManagementService.cs
+++ BookManagementSystem.Services/Interfaces/IUserManagementService.cs
@@ -3,12 +3,12 @@ using BookManagementSystem.Domain.Entities;
 
 namespace BookManagementSystem.Service.Interfaces
 {
-    public interface IUserManagementService
-    {
-        Task<Common> Register(RegisterRequest register);
-        Task<LoginResponse> Login(LoginRequest login);
-        Task<OptResponse> OtpManager(string email);
-        Task<Common> OtpVerify(OtpVerifyRequest request);
-        Task<Common> UpdatePassword();
-    }
+	public interface IUserManagementService
+	{
+		Task<Common> Register(RegisterRequest register);
+		Task<LoginResponse> Login(LoginRequest login);
+		Task<OptResponse> OtpManager(string email);
+		Task<Common> OtpVerify(OtpVerifyRequest request);
+		Task<Common> UpdatePassword();
+	}
 }
